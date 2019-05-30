31bac1c0b8fe8335704e9ac0c02aca4bf778b185
Washington Andres De Vera Gonzalez <wadevera@gmail.com>
2019-05-30 18:06:03 -0300

    ejb con alta camion

--- sgrm/sgrm-ejb/src/main/java/negocio/CContenedoresyZonas.java
+++ sgrm/sgrm-ejb/src/main/java/negocio/CContenedoresyZonas.java
@@ -1,10 +1,16 @@
 package negocio;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import javax.ejb.EJB;
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
+
+import datatypes.DtZona;
+import persistencia.MemoryLocal;
 
 
 
@@ -15,7 +21,9 @@ import javax.ejb.Stateless;
 @Stateless
 @LocalBean
 public class CContenedoresyZonas implements CContenedoresyZonasRemote, CContenedoresyZonasLocal {
-
+	
+	@EJB
+	MemoryLocal mem;
 	
 	private Map<Long,Zona> zonas = new HashMap<>();
 	private Map<Long,Contenedor> contenedores = new HashMap<>();
@@ -33,5 +41,24 @@ public class CContenedoresyZonas implements CContenedoresyZonasRemote, CContened
 		else
 			return false;
 	}
+	
+    public List<DtZona> obtenerZonas(){
+    	List<Zona> zonas = mem.getAllZonas();
+    	List <DtZona> dtzonas= new ArrayList<DtZona>();
+		for (Zona z: zonas) {
+			DtZona dtz = z.obtenerDtZona();
+			dtzonas.add(dtz);
+		}
+		return dtzonas;
+    }
+    
+    public String altaCamion(String matricula, long idZona) {
+		Camion camion = new Camion(matricula);
+		Zona zona = mem.buscarZona(idZona);
+		zona.agregarACuadrilla(camion);
+		mem.altaCamion(camion);
+    	return "Se agrego el camion " + camion.getIdCamion() +" a la cuadrilla de la zona "+ zona.getIdZona();
+    	
+    }
 
 }
--- sgrm/sgrm-ejb/src/main/java/persistencia/MemoryLocal.java
+++ sgrm/sgrm-ejb/src/main/java/persistencia/MemoryLocal.java
@@ -1,9 +1,13 @@
 package persistencia;
 
+import java.util.List;
+
 import javax.ejb.Local;
 
+import negocio.Camion;
 import negocio.Final;
 import negocio.Usuario;
+import negocio.Zona;
 import negocio.ZonaEstado;
 
 @Local
@@ -14,4 +18,7 @@ public interface MemoryLocal {
 	public abstract boolean altaGestor(String nombre, String apellido, String nickAdmin);
 	public abstract boolean altaZonaGestor(long idZona, ZonaEstado zEstado, String mail);
 	public abstract Usuario findUsuario(String mail);
+	public void altaCamion(Camion camion);
+	public abstract Zona buscarZona(long idZona);
+	public abstract List<Zona> getAllZonas();
 }
